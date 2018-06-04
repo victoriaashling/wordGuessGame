27682a13eb40cc2d136ab8a0c7eeb71f20c6b7ba
victoriaashling <victoria.ashling@gmail.com>
2018-06-03 23:06:00 -0400

    created function to shuffle array, so that each word is seen once per cycle, but in random order

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,38 @@
 $(document).ready(function() {
 
-    // Set global variables that will need to be accessed but not over-written in any of the code.
-    var words = ["hello", "bonjour", "annyeong", "hola"];
+    // Set global variables that will need to be accessed in multiple parts of the code.
+    var words = ["japan", "germany", "uzbekistan", "lebanon", "nigeria", "singapore", "bangladesh", "ireland", "madagascar", "ethiopia", "canada", "belize", "mauritania", "vanuatu", "uruguay", "belgium", "latvia", "mozambique", "panama", "turkey"];
     var blankString = "______________________"
     var validLetters = "abcdefghijklmnopqrstuvwxyz";
     var wins = 0;
     var losses = 0;
+    var guessesRemaining = 10;
+
+    function shuffle() {
+        for (var i = 0; i < words.length; i++) {
+            var temp = words[i];
+            var randomNumber = Math.floor(Math.random() * words.length);
+            words[i] = words[randomNumber];
+            words[randomNumber] = temp;
+        };
+        console.log(words);
+    };
+
+    shuffle();
+
+    var nextWord = words.length - 1;
+    var currentWord = words[nextWord];
+    var blankPart = blankString.substring(0, currentWord.length);
+
 
     // Set a reset function that will update or reset (each as necessary) info each time you win or lose.
-    function reset() {
-        currentWord = words[Math.floor(Math.random() * words.length)];
+    function resetPage() {
+        nextWord = nextWord + 1;
+        if (nextWord === words.length) {
+            shuffle();
+            nextWord = 0;
+        };
+        currentWord = words[nextWord];
         blankPart = blankString.substring(0, currentWord.length);
         $("#blankWord").text(blankPart);
         guessesRemaining = 10;
@@ -19,7 +42,7 @@ $(document).ready(function() {
         $("#wins").text("Wins: " + wins);
     };
 
-    reset();
+    resetPage();
 
     
     document.onkeyup = function(event) {
@@ -47,13 +70,17 @@ $(document).ready(function() {
         if (guessesRemaining < 1) {
             alert('You Lose! The correct word was "' + currentWord + '."');
             losses = losses + 1;
-            reset();            
+            resetPage();            
         };
         
         if (blankPart.includes("_") === false) {
-            alert('You win! The correct word was "' + currentWord + '."');
-            wins = wins + 1;
-            reset();
+            setTimeout(function () {
+                alert('You win! The correct word was "' + currentWord + '."');
+                wins = wins + 1;
+                resetPage();
+            }, 100);
+
+            
         };
     };
 });
